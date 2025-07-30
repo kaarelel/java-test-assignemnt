6013fcec81a582099738ffe018f33c957a5de76d
kaarelel <kaarelel@tlu.ee>
2025-07-30 18:35:35 +0300

    task 05 done added average calculation

--- java-pizza-test-assignment-master/src/main/java/com/nortal/pizza/repository/OrderRepository.java
+++ java-pizza-test-assignment-master/src/main/java/com/nortal/pizza/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<OrderEntity, Integer> {
@@ -13,7 +14,9 @@ public interface OrderRepository extends CrudRepository<OrderEntity, Integer> {
 	@Query("SELECT o from OrderEntity o WHERE o.client.username=:username ORDER BY o.id DESC")
 	List<OrderEntity> findOrdersByClientId(@Param("username") String username);
 
-	@Query("SELECT o from OrderEntity o WHERE LOWER(o.address) LIKE %:aadress% ORDER BY o.id DESC")
-	List<OrderEntity> findByOrdersAadress(@Param("aadress") String aadress);
+	@Query("SELECT o from OrderEntity o WHERE LOWER(o.address) LIKE %:address% ORDER BY o.id DESC")
+	List<OrderEntity> findByOrdersAddress(@Param("address") String address);
 
+	@Query("SELECT AVG(o.price) FROM OrderEntity o WHERE o.client.id = :clientId")
+	BigDecimal findAveragePriceByClientId(@Param("clientId") Integer clientId);
 }
